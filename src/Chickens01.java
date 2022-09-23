c4577baa45c63d3647f711db4002d8189a39f9dc
JoshLavergne <113380975+JoshLavergne@users.noreply.github.com>
2022-09-23 13:49:53 -0400

    My Finished Chicken Project

--- src/Chickens01.java
+++ src/Chickens01.java
@@ -1,7 +1,29 @@
 public class Chickens01 {
     public static void main(String[] args) {
         //Put your code here
+    	
+    	//   Beginning & Variables  /*
+    	
+    	int totalEggs = 0;
+        int chickenCount = 8;
+        int eggsPerChicken = 4;
+    
+         //  Monday    /*
+
+        totalEggs = eggsPerChicken * chickenCount;
+        
+        
+        //  Tuesday    /*
+        
+        chickenCount++;
+        totalEggs += eggsPerChicken * chickenCount;
+        
+        //  Wednesday    /*
+        
+        chickenCount /= 2;
+        totalEggs += eggsPerChicken * chickenCount;
         
         System.out.println(totalEggs);
+        
     }   
 }
--- src/Chickens02.java
+++ src/Chickens02.java
@@ -1,9 +1,36 @@
 public class Chickens02 {
     public static void main(String[] args) {
         //Put your code here
+    	
+    	// Starting & Variables /*
+    	
+    	double dailyAverage = 0;
+    	int eggCount = 0;
+    	double monthlyAverage = 0;
+    	double monthlyProfit = 0;
 
+    	// Monday /*
+    	
+    	eggCount+= 100;
+    	
+    	// Tuesday /*
+    	
+    	eggCount+= 121;
+    	
+    	// Wednesday /*
+    	
+    	eggCount+= 117;
+    	
+    	dailyAverage = eggCount/3.0;
+    	
         System.out.println("Daily Average:   " +dailyAverage);
+       
+        monthlyAverage = dailyAverage*30;
+        
         System.out.println("Monthly Average: " +monthlyAverage);
+        
+        monthlyProfit = monthlyAverage*0.18;
+        
         System.out.println("Monthly Profit:  $" +monthlyProfit);
     }
     
